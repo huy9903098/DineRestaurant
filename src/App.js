05b013352a9832efc25d3d76b158f18fc1c0e468
Huy <skyzerzlol@gmail.com>
2021-03-01 04:50:00 +0200

    adjust scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.scss';
 import Homepage from './Components/Homepage/index';
+import Booking from './Components/Booking/index';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/booking" component={Booking} />
+      </div>
+    </Router>
   );
 }
 
